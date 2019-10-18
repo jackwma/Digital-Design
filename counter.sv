8787a9e0580c198ffe4526ae513f89f8cba08728
Jack <44331391+jackwma@users.noreply.github.com>
2019-10-17 17:47:34 -0700

    lab 1

---
+++ counter.sv
@@ -0,0 +1,85 @@
+module counter#(parameter WIDTH=4) (out, inc, dec, reset, clk, count_num1, count_num2);
+	output logic [WIDTH:0] out;
+	output logic [WIDTH-1:0] count_num1, count_num2;
+	input logic inc, dec, reset, clk;
+	 
+	 //post: this block is to sure the counters will be able to increment the counters within the range correctly.
+	 always_ff @(posedge clk) begin
+		if(reset) begin // case for default start
+			count_num1 <= 0;
+			count_num2 <= 0;
+			out <= 0;
+		end
+		else if(inc && out < 25) begin // within the range of 25 and incrementing
+			out <= out + 1'b1;
+			if(count_num1 < 9) begin // within 9 so that it doesn't use the second counter
+				count_num1 <= count_num1 + 1'b1;
+			end
+			else begin //
+				if(count_num2 < 9) begin
+					count_num2 <= count_num2 + 1'b1;
+				end
+				else begin
+					count_num2 <= 0;
+					count_num1 <= 0;
+				end
+			end
+		end
+		else if(dec && out > 0) begin // the counter is positive so it is okay to decrement
+			out <= out - 1'b1;
+			if(count_num1 > 0) begin 
+				count_num1 <= count_num1 - 1;
+			end
+			else begin
+				if(count_num2 > 0) begin
+					count_num2 <= count_num2 - 1;
+				end
+				else begin
+					count_num2 <= 9;
+					count_num1 <= 9;
+				end
+			end
+		end
+	  end 
+endmodule
+
+
+module counter_testbench#(parameter WIDTH=4) ();  
+	logic [WIDTH:0] out;
+	logic [WIDTH-1:0] count_num1, count_num2;
+	logic inc, dec, reset, clk;
+	 
+	counter dut( .out, .inc, .dec, .reset, .clk, .count_num1, .count_num2);      
+	// Set up the clock.  
+	parameter CLOCK_PERIOD = 100;  
+	initial begin
+	 clk <= 1; 
+	 forever #( CLOCK_PERIOD / 2 ) clk <= ~clk;  
+	end      
+	// Set up the inputs to the design.  Each line is a clock cycle.  
+	initial begin                        
+					                     @(posedge clk);    
+		reset <= 1;					      @(posedge clk);    
+		reset <= 0;                   @(posedge clk);                        
+					  inc <= 1; dec <=0; @(posedge clk);                        
+					  inc <= 1; dec <=0; @(posedge clk);                        
+					  inc <= 0; dec <=1; @(posedge clk);                        
+					  inc <= 1; dec <=0; @(posedge clk);                        
+					  inc <= 1; dec <=0; @(posedge clk);                        
+					  inc <= 0; dec <=1; @(posedge clk);                        
+					  inc <= 0; dec <=1; @(posedge clk);                        
+					  inc <= 1; dec <=0; @(posedge clk);                        
+					  inc <= 1; dec <=0; @(posedge clk);                        
+					  inc <= 1; dec <=0; @(posedge clk);                        
+												@(posedge clk);  
+		           inc <= 1; dec <=0; @(posedge clk);                        
+												@(posedge clk);  
+												@(posedge clk);
+												@(posedge clk);  
+					  inc <= 1; dec <=0; @(posedge clk);                        
+												@(posedge clk);
+												@(posedge clk);
+												@(posedge clk);     
+		$stop; // End the simulation.  
+	end 
+endmodule
---
+++ sensor.sv
@@ -0,0 +1,120 @@
+module sensor (clk, reset, C, D, enter, exit);
+	input logic clk, reset, C, D;
+	output logic enter, exit;
+	
+	//creating a ps, ns case for these 4 states
+	enum {EMPTY, A, B, AB} ps, ns;
+	
+	//post: this block is to ensure that the inputs will be able to change states within correctly.
+	always_comb begin
+		case(ps)	
+		EMPTY: // EMPTY case
+			if(C&~D) ns = A;
+			else if(~C&D) ns = B;
+			else ns = EMPTY;
+		A: // case for A
+			if(C&~D) ns = A;
+			else if(C&D) ns = AB;
+			else ns = EMPTY;		// Dont care, go to EMPTY
+		B: // case for B
+			if(~C&D) ns = B;
+			else if(C&D) ns = AB;
+			else ns = EMPTY;		// Dont care, go to EMPTY
+		AB: // case for A and B
+			if(C&D) ns = AB;
+			else if(C&~D) ns = A;
+			else if(~C&D) ns = B;
+			else ns = EMPTY;
+		endcase
+	end
+	
+	//post: block to record inputs; will be able to record the input and change its states with enter and exit. 
+	always_comb begin
+		case(ps) 
+		AB:
+			if(~C&D)
+				begin 
+					enter = 1'b1;
+					exit = 1'b0;
+				end
+			else if(C&~D)
+				begin
+					exit = 1'b1;
+					enter = 1'b0;
+				end
+			else 
+				begin
+					enter = 1'b0;
+					exit = 1'b0;
+				end
+		default: 
+			begin 
+				enter = 1'b0;
+				exit = 1'b0;
+			end
+		endcase
+	end
+	
+	//DFF to eliminate metability
+	always_ff @(posedge clk) begin
+		if(reset)
+			ps <= EMPTY;
+		else
+			ps <= ns;
+	end
+endmodule
+
+module sensor_testbench();
+	logic clk, reset, a, b, enter, exit;
+	
+	sensor dut(.clk, .reset, .C(a), .D(b), .enter, .exit);
+	
+	parameter CLOCK_PERIOD = 100;
+	initial begin
+		clk <= 0;
+		forever #(CLOCK_PERIOD/2) clk <= ~clk;
+	end
+	
+	initial begin
+		reset <= 1; a <= 1; b <= 1; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 0; b <= 0; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);		
+		reset <= 0; 	 @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 1; b <= 0; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 1; b <= 1; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 0; b <= 0; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 1; b <= 0; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 0; b <= 0; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 1; b <= 1; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 1; b <= 1; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 1; b <= 0; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		a <= 0; b <= 0; @(posedge clk);
+		@(posedge clk);
+		@(posedge clk);
+		$stop;
+	end
+	
+endmodule 
+	
+	
